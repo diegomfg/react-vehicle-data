6aff512fb1742d98e69cd376de053810f12ea113
Diego Matheus <diegoamatheus@gmail.com>
2022-07-19 14:36:50 -0400

    initial config, install tailwind css, define pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+const BASE_URL = "https://vpic.nhtsa.dot.gov/api/vehicles";
+
 
 function App() {
+  const fetch = (e)=>{
+    return axios.get(BASE_URL+'/getmanufacturerdetails/honda?format=json').then((result)=>{
+      console.log("RESULT: ", result.data.Results)
+    }).catch((error) => {
+      console.log("ERROR: ", error);
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={fetch}>Click Me</button>
     </div>
   );
 }
